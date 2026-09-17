31904b3d342a9d6b837ca5b5e56f8accbcd1e254
NeuroSaiKou <3065441861@qq.com>
2026-09-17 15:07:27 +0800

    fix:move page background to root element

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -3,7 +3,7 @@ import Profile from "@/components/profile";
 
 export default function About() {
   return (
-    <div className="flex flex-1 flex-col gap-6 bg-zinc-50 p-6 font-sans lg:px-8 dark:bg-black">
+    <div className="flex flex-1 flex-col gap-6 p-6 font-sans lg:px-8">
       <Banner imageSrc="/banner.jpg" />
       <Profile />
     </div>
